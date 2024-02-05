7b7c48a5d23f96321994e7c97742e874d05684ab
ryan55288 <ryan804491@yahoo.com.tw>
2024-02-05 16:03:42 +0800

    維護最新消息SCSS寫法

--- src/view/News.vue
+++ src/view/News.vue
@@ -3,8 +3,8 @@
 <template>
   <div class="box1"></div>
   <section class="section1">
-    <h3>公告專區</h3>
-    <div class="d-flex pt-60">
+    <h3 class="">公告專區</h3>
+    <div class="d-flex pt-60 justify-content-center">
       <div><img src="../assets/img/bg/news/1.svg" alt="" /></div>
       <div class="newCard ml-30">
         <h4>年末大清倉 單抽最低99！交換禮物福袋陸續登場！</h4>
@@ -18,7 +18,7 @@
     <div class="d-flex justify-content-center">
       <hr />
     </div>
-    <div class="d-flex pt-60">
+    <div class="d-flex pt-60 justify-content-center">
       <div><img src="../assets/img/bg/news/2.svg" alt="" /></div>
       <div class="newCard ml-30">
         <h4>【得獎公告】雙11店家加碼&享玩拉麵活動 得獎名單</h4>
@@ -32,7 +32,7 @@
     <div class="d-flex justify-content-center">
       <hr />
     </div>
-    <div class="d-flex pt-60">
+    <div class="d-flex pt-60 justify-content-center">
       <div><img src="../assets/img/bg/news/2.svg" alt="" /></div>
       <div class="newCard ml-30">
         <h4>年末大清倉 單抽最低99！交換禮物福袋陸續登場！</h4>
@@ -45,66 +45,48 @@
     <div class="d-flex justify-content-center">
       <hr />
     </div>
-
-    <!-- <div class="container ml-30 pt-60">
-      <div class="my-row">
-        <div class="col-lg-4">
-          <img src="../assets/img/bg/news/1.svg" alt="" />
-        </div>
-        <div class="col-lg-8 newCard">
-          <p>
-            ✨好康一、交換禮物福袋coming soon
-            12月將陸續推出500及1000元的福袋/爽抽樂,<br />敬請期待🔥
-          </p>
-          <p>✨好康二、年末大清倉 即日(12/1)起~12月底，年末清倉特賣🔥</p>
-        </div>
-      </div>
-    </div> -->
   </section>
 </template>
 <style scoped>
-section img {
-  margin-left: 40px;
-}
-.section1 hr {
-  margin-top: 40px;
-  width: 1700px;
-  text-align: center;
-  border: 3px gray solid;
-  border-radius: 5px;
-}
-.newCard {
-  background: #fff;
-  width: 1000px;
-  height: 255px;
-  margin-left: 180px;
-  flex-wrap: nowrap;
-}
-.section1 p {
-  margin-top: 10px;
-  font-size: 24px;
-  font-weight: bold;
-  padding: 0px 40px 0px 40px;
-  line-height: 35px;
-}
-.section1 h4 {
-  font-size: 36px;
-  font-weight: bold;
-  padding: 40px 0px 20px 40px;
-}
-.section1 h3 {
-  margin-left: 60px;
-  padding-top: 140px;
-  border-bottom: 5px solid #000;
-  font-size: 42px;
-  font-weight: bold;
-  width: 190px;
+.section1 {
+  .newCard {
+    background: #fff;
+    width: 1000px;
+    height: 255px;
+    margin-left: 180px;
+    flex-wrap: nowrap;
+  }
+  background-color: var(--orange-color2);
+  height: 1400px;
+  hr {
+    margin-top: 40px;
+    width: 1700px;
+    text-align: center;
+    border: 3px gray solid;
+    border-radius: 5px;
+  }
+  p {
+    margin-top: 10px;
+    font-size: 24px;
+    font-weight: bold;
+    padding: 0px 40px 0px 40px;
+    line-height: 35px;
+  }
+  h4 {
+    font-size: 36px;
+    font-weight: bold;
+    padding: 40px 0px 20px 40px;
+  }
+  h3 {
+    margin-left: 250px;
+    padding-top: 140px;
+    border-bottom: 5px solid #000;
+    font-size: 42px;
+    font-weight: bold;
+    width: 190px;
+  }
 }
 
-.section1 {
-  background-color: var(--orange-color2);
-  height: 1500px;
-}
 .box1 {
   width: 100%;
   height: 80px;
