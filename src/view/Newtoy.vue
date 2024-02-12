7cef88c6430c2f247f377bb1acba8976a1f97568
sam_wang <sam_wang@pxmart.com.tw>
2024-02-12 23:34:18 +0800

    feature: 新潮玩具商品建檔

--- src/view/Newtoy.vue
+++ src/view/Newtoy.vue
@@ -89,18 +89,22 @@
     </div>
   </section>
 
-  <section class="section2"><NewProductType class="pt-20" /></section>
+  <section class="productType">
+    <ul class="d-flex justify-content-center align-items-center h-100">
+      <li v-for="cate in category" :key="cate.id" @click="currentSelectCategory = cate">{{ cate.title }}</li>
+    </ul>
+  </section>
 
   <section class="section3">
     <div
       class="text-white text-center fw-bold fs-36 pt-40 animate__animated animate__flipInX"
     >
-      鋼彈模型
+      {{ currentSelectCategory.title }}
     </div>
     <div class="container">
       <div class="row">
-        <div v-for="prod in productList" class="col-md-6 col-lg-3 p-10">
-          <NewProductCard :productDetail="prod" />
+        <div v-for="prod in formatProductByCategory" :key="prod.id" class="col-md-6 col-lg-3 p-10">
+          <ProductCard :productDetail="prod" />
         </div>
       </div>
     </div>
@@ -123,7 +127,7 @@
       <div
         id="oneReward"
         class="reel-body mb-200 animate__animated"
-        :class="{ animate__bounceInDown: shouldAnimate }"
+        :class="{ animate__bounceInDown: true }"
       >
         <router-link to="OneReward/" class="text-decoration-none">
           <div class="d-flex justify-content-center pt-50">
@@ -189,155 +193,442 @@
   </section>
 </template>
 
-<script>
-import NewProductType from "@/components/NewProductType.vue";
-import NewProductCard from "@/components/NewProductCard.vue";
-import Anchor from "@/components/Anchor.vue";
-import Page from "@/components/Page.vue";
-export default {
-  components: { NewProductType, NewProductCard, Anchor, Page },
+<script setup>
+import { reactive } from 'vue';
 
-  data() {
-    return {
-      shouldAnimate: false,
-      productList: [
-        {
-          id: 1,
-          name: "鋼彈模型-紅色異端",
-          stock: 10,
-          price: 680,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-1.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 2,
-          name: "鋼彈模型-飛翼零式",
-          stock: 13,
-          price: 700,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/Rectangle 6.svg  ",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-爆喪女妖",
-          stock: 15,
-          price: 1400,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-3.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-OO鋼彈",
-          stock: 1,
-          price: 820,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-獨角獸",
-          stock: 8,
-          price: 1040,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-2.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-自由鋼彈",
-          stock: 1,
-          price: 699,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-4.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-RX-78-2",
-          stock: 1,
-          price: 680,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-5.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-        {
-          id: 3,
-          name: "鋼彈模型-命運鋼彈",
-          stock: 1,
-          price: 565,
-          imgurl: new URL(
-            "../assets/img/Product/newtoy/鋼彈模型/IMG-6.svg",
-            import.meta.url
-          ),
-          iconSrc: new URL(
-            "../assets/img/logo&icon/mylove.svg",
-            import.meta.url
-          ),
-          quantity: 0,
-        },
-      ],
-    };
+const category = reactive([
+  {
+    id: 6,
+    title: '鋼彈模型',
   },
-  mounted() {
-    $(window).scroll(this.handleScroll);
+  {
+    id: 7,
+    title: '模型車',
   },
-  beforeUnmount() {
-    $(window).off("scroll", this.handleScroll);
+  {
+    id: 8,
+    title: 'LEGO',
   },
-  methods: {
-    handleScroll() {
-      const section4Rect = this.$refs.section4.getBoundingClientRect();
-      this.shouldAnimate =
-        section4Rect.top < window.innerHeight && section4Rect.bottom > 0;
-    },
+  {
+    id: 9,
+    title: '漫威',
   },
-};
+  {
+    id: 10,
+    title: '洛克人',
+  },
+])
+const currentSelectCategory = ref(category[0])
+
+const productList = reactive([
+  {
+    id: 54,
+    name: "鋼彈模型- 飛翼零式",
+    stock: 1,
+    price: 680,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 55,
+    name: "鋼彈模型- OO鋼彈",
+    stock: 1,
+    price: 820,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 56,
+    name: "RG 019 紅色異端",
+    stock: 1,
+    price: 680,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 57,
+    name: "鋼彈模型- 獨角獸鋼彈",
+    stock: 1,
+    price: 1040,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 58,
+    name: "鋼彈模型- 爆喪女妖",
+    stock: 1,
+    price: 1400,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 59,
+    name: "鋼彈模型-自由鋼彈",
+    stock: 1,
+    price: 699,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 60,
+    name: "鋼彈模型-RX-78-2",
+    stock: 1,
+    price: 680,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 61,
+    name: "鋼彈模型-命運鋼彈",
+    stock: 1,
+    price: 565,
+    picture: new URL("../assets/img/Product/center/IMG-4.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 9,
+    name: "鋼彈模型-翔翼攻擊",
+    stock: 12,
+    price: 600,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 62,
+    name: "鋼彈模型- 海牛",
+    stock: 5,
+    price: 945,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 63,
+    name: "鋼彈模型-逆襲的夏亞",
+    stock: 5,
+    price: 970,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 6
+  },
+  {
+    id: 64,
+    name: "模型車-豐田AE86",
+    stock: 12,
+    price: 300,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 65,
+    name: "模型車- 特斯拉",
+    stock: 5,
+    price: 250,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 66,
+    name: "模型車-阿提斯",
+    stock: 5,
+    price: 335,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 67,
+    name: "模型車-HONDA 4",
+    stock: 12,
+    price: 1000,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 68,
+    name: "模型車- 馬自達RX7",
+    stock: 3,
+    price: 490,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 69,
+    name: "模型車-AMG 賓士",
+    stock: 5,
+    price: 188,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 70,
+    name: "模型車-模型悍馬車",
+    stock: 8,
+    price: 150,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 71,
+    name: "模型車 -保時捷",
+    stock: 5,
+    price: 260,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 72,
+    name: "模型車-焰之魔神",
+    stock: 12,
+    price: 650,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 73,
+    name: "模型車- 合金跑車寶馬i8",
+    stock: 5,
+    price: 1000,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 7
+  },
+  {
+    id: 74,
+    name: "LEGO-龍年限定",
+    stock: 1,
+    price: 450,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 75,
+    name: "LEGO-中世紀古堡",
+    stock: 1,
+    price: 1300,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 10,
+    name: "LEGO-冰忍的強化機械人",
+    stock: 1,
+    price: 299,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 76,
+    name: "LEGO -阿光的鈦機械人",
+    stock: 1,
+    price: 2400,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 77,
+    name: "LEGO-泰坦機器人",
+    stock: 12,
+    price: 770,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 78,
+    name: "LEGO- 水晶國王",
+    stock: 5,
+    price: 1599,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 79,
+    name: "LEGO- 機械人摩托飛車",
+    stock: 5,
+    price: 1465,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 80,
+    name: "LEGO- 忍者小隊機械人",
+    stock: 5,
+    price: 2575,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 8
+  },
+  {
+    id: 81,
+    name: "漫威-漫威蜘蛛人",
+    stock: 1,
+    price: 319,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 82,
+    name: "漫威-美國隊長",
+    stock: 1,
+    price: 1300,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 83,
+    name: "漫威-死侍",
+    stock: 1,
+    price: 299,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 84,
+    name: "漫威 -鋼鐵人",
+    stock: 1,
+    price: 2400,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 85,
+    name: "漫威-金鋼狼",
+    stock: 12,
+    price: 770,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 86,
+    name: "漫威- 八爪博士",
+    stock: 5,
+    price: 1599,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 87,
+    name: "漫威- 雷神之鎚",
+    stock: 5,
+    price: 1349,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 88,
+    name: "漫威- 星爵",
+    stock: 5,
+    price: 775,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 9
+  },
+  {
+    id: 11,
+    name: "洛克人-黑暗EXE",
+    stock: 1,
+    price: 1550,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 89,
+    name: "洛克人- 第二裝甲",
+    stock: 1,
+    price: 1650,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 90,
+    name: "洛克人-Premium",
+    stock: 1,
+    price: 3050,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 91,
+    name: "洛克人 -第四裝甲",
+    stock: 1,
+    price: 1850,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 92,
+    name: "洛克人-流星洛克人",
+    stock: 12,
+    price: 1849,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 93,
+    name: "洛克人- 八爪博士",
+    stock: 5,
+    price: 1599,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 94,
+    name: "洛克人- Rockman 11",
+    stock: 5,
+    price: 1680,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+  {
+    id: 95,
+    name: "洛克人- MAX ARMOR",
+    stock: 5,
+    price: 1980,
+    picture: new URL("../assets/img/Product/center/IMG-5.svg", import.meta.url),
+    qty: 0,
+    categoryId: 10
+  },
+])
+
+const formatProductByCategory = computed(() => productList.filter(prod => prod.categoryId === currentSelectCategory.value.id))
 </script>
 
 <style lang="scss" scoped>
@@ -412,7 +703,7 @@ export default {
   }
 }
 .section3 {
-  height: 1300px;
+  min-height: 1300px;
   background-color: var(--green-color1);
   .row {
     padding-top: 40px;
@@ -454,6 +745,36 @@ export default {
 @media (max-width: 1440px) {
   .Anchor {
     display: none;
+  }
+}
+.productType {
+  height: 80px;
+  width: 100%;
+  background-color: var(--orange-color2);
+  font-family: "Lemon";
+  li {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 170px;
+    height: 50px;
+    background-color: var(--orange-color3);
+    color: #fff;
+    font-weight: bold;
+    font-size: 24px;
+    border-radius: 60px;
+    margin-right: 20px;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+      rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+    cursor: pointer;
+    &:hover {
+      background-color: var(--orange-color1);
+      color: var(--orange-color2);
+    }
+    &:active {
+      background-color: var(--orange-color2);
+      color: var(--orange-color3);
+    }
   }
 }
 </style>
