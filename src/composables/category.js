3f25d83ee4dca217998e27bef5ead932a18733f5
ryan55288 <ryan804491@yahoo.com.tw>
2024-02-17 21:57:32 +0800

    style:更新新潮玩具商品&背景&標題字體

--- src/composables/category.js
+++ src/composables/category.js
@@ -1,89 +1,95 @@
 export function useCategory(type) {
-  const route = useRoute()
+  const route = useRoute();
   const grade8Category = reactive([
     {
       id: 1,
-      title: '徽章戰士',
-      path: '/Grade8',
-      section: '#grade8'
+      title: "徽章戰士",
+      path: "/Grade8",
+      section: "#grade8",
     },
     {
       id: 2,
-      title: '百獸戰隊',
-      path: '/Grade8',
-      section: '#grade8'
+      title: "百獸戰隊",
+      path: "/Grade8",
+      section: "#grade8",
     },
     {
       id: 3,
-      title: '彈珠超人',
-      path: '/Grade8',
-      section: '#grade8'
+      title: "彈珠超人",
+      path: "/Grade8",
+      section: "#grade8",
     },
     {
       id: 4,
-      title: '戰鬥陀螺',
-      path: '/Grade8',
-      section: '#grade8'
+      title: "戰鬥陀螺",
+      path: "/Grade8",
+      section: "#grade8",
     },
     {
       id: 5,
-      title: '數碼寶貝',
-      path: '/Grade8',
-      section: '#grade8'
+      title: "數碼寶貝",
+      path: "/Grade8",
+      section: "#grade8",
     },
-  ])
+  ]);
   const newToyCategory = reactive([
     {
       id: 6,
-      title: '鋼彈模型',
-      path: '/Newtoy',
-      section: '#newToy'
+      title: "鋼彈模型",
+      path: "/Newtoy",
+      section: "#newToy",
     },
     {
       id: 7,
-      title: '模型車',
-      path: '/Newtoy',
-      section: '#newToy'
+      title: "模型車",
+      path: "/Newtoy",
+      section: "#newToy",
     },
     {
       id: 8,
-      title: 'LEGO',
-      path: '/Newtoy',
-      section: '#newToy'
+      title: "LEGO",
+      path: "/Newtoy",
+      section: "#newToy",
     },
     {
       id: 9,
-      title: '漫威',
-      path: '/Newtoy',
-      section: '#newToy'
+      title: "漫威",
+      path: "/Newtoy",
+      section: "#newToy",
     },
     {
       id: 10,
-      title: '洛克人',
-      path: '/Newtoy',
-      section: '#newToy'
+      title: "洛克人",
+      path: "/Newtoy",
+      section: "#newToy",
     },
-  ])
+  ]);
   const oneRewardCategory = reactive([
     {
       id: 11,
-      title: '一番賞',
-      path: '/OneReward',
-      section: '#oneReward'
-    }
-  ])
-  const allCategory = [...grade8Category, ...newToyCategory, ...oneRewardCategory]
+      title: "一番賞",
+      path: "/OneReward",
+      section: "#oneReward",
+    },
+  ]);
+  const allCategory = [
+    ...grade8Category,
+    ...newToyCategory,
+    ...oneRewardCategory,
+  ];
   const categoryType = () => {
     switch (type) {
-      case 'grade8':
-        return grade8Category[0]
-      case 'newToy':
-        return newToyCategory[0]
+      case "grade8":
+        return grade8Category[0];
+      case "newToy":
+        return newToyCategory[0];
     }
-  }
+  };
   const currentSelectCategory = ref(
-    route.query.keyword ? { title: route.query.keyword, id: +route.query.id } : categoryType()
-    )
+    route.query.keyword
+      ? { title: route.query.keyword, id: +route.query.id }
+      : categoryType()
+  );
 
   watch(
     () => route.query,
@@ -92,14 +98,14 @@ export function useCategory(type) {
         currentSelectCategory.value = {
           title: route.query.keyword,
           id: +route.query.id,
-        }
+        };
       }
     }
-  )
+  );
   return {
     grade8Category,
     newToyCategory,
     currentSelectCategory,
-    allCategory
-  }
+    allCategory,
+  };
 }
