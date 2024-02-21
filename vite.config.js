657fffd1b89e36152e4043b003f6eb6d19c03dcd
sam_wang <sam_wang@pxmart.com.tw>
2024-02-21 20:34:41 +0800

    config: 壓縮圖片插件

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 import path from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from 'unplugin-vue-components/vite'
+import imagemin from 'unplugin-imagemin/vite';
 
 export default defineConfig({
   plugins: [
@@ -19,7 +20,29 @@ export default defineConfig({
     Components({
       dirs: ['src/components'],
       extensions: ['vue'],
-    })
+    }),
+    imagemin({
+      mode: "sharp",
+      compress: {
+        jpeg: {
+          // 0 ~ 100
+          quality: 25,
+        },
+        png: {
+          // 0 ~ 100
+          quality: 25,
+        },
+        webp: {
+          // 0 ~ 100
+          quality: 25,
+        },
+      },
+      conversion: [
+        { from: "png", to: "webp" },
+        { from: "jpeg", to: "png" },
+      ],
+      cache: false,
+    }),
   ],
   resolve: {
     alias: [
